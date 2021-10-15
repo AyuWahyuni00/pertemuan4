451a0a2cb245ffd708756609f46c69fd4aebd322
AyuWahyuni00 <85227419+AyuWahyuni00@users.noreply.github.com>
2021-10-15 23:45:21 +0700

    Add files via upload

---
+++ Latihanke4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanke4;
+import UI.Home;
+
+/**
+ *
+ * @author riki
+ */
+public class Latihanke4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home fHome = new Home();
+        fHome.setVisible(true);
+    }
+    
+}
